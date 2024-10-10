90c51260fe8c9666d03bec839abc765604ae12ea
guillaumebeysson <guillaume.beysson@gmail.com>
2024-10-10 13:20:14 +0200

    add mat modules in tests

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,13 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {JokesService} from "./services/jokes.service";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { JokesService } from "./services/jokes.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatDividerModule,
+        MatButtonModule,
+        MatIconModule
+      ],
       providers: [JokesService]
     }).compileComponents();
   });
